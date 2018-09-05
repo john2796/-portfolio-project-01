4ac762b9333039f8216253acd82d221afc38ec09
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-09-04 20:21:49 -0700

    peace out
    Signed-off-by: Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>

--- online-store-1/src/App.js
+++ online-store-1/src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import Router from "./Router";
+
+const Navigation = props => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/cart">Cart</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App page-container ">
+        <Navigation />
+        <Router />
       </div>
     );
   }
