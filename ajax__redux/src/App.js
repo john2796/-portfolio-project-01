d98f02b3a034668e9220d08bc6c1d6785b19be67
john2796 <Benedictmiranda27@yahoo.com>
2018-12-30 18:03:06 -0800

    added paragraph

--- ajax__redux/src/App.js
+++ ajax__redux/src/App.js
@@ -9,6 +9,36 @@ class App extends Component {
         <h1>Repos Search</h1>
         <TestFile />
         <h2>testing testfile </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          suscipit voluptates corporis, perferendis at dolore totam molestias
+          tempore dignissimos et minus laborum esse similique ipsam odit aliquam
+          nisi labore nam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          suscipit voluptates corporis, perferendis at dolore totam molestias
+          tempore dignissimos et minus laborum esse similique ipsam odit aliquam
+          nisi labore nam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          suscipit voluptates corporis, perferendis at dolore totam molestias
+          tempore dignissimos et minus laborum esse similique ipsam odit aliquam
+          nisi labore nam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          suscipit voluptates corporis, perferendis at dolore totam molestias
+          tempore dignissimos et minus laborum esse similique ipsam odit aliquam
+          nisi labore nam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          suscipit voluptates corporis, perferendis at dolore totam molestias
+          tempore dignissimos et minus laborum esse similique ipsam odit aliquam
+          nisi labore nam?
+        </p>
       </div>
     );
   }
