2cd057de02439dc4bd2f8b1459e740ae283469e5
john2796 <Benedictmiranda27@yahoo.com>
2019-02-10 16:02:50 -0800

    adding card on property content

--- portfolio-003-real-estate-/client/src/components/properties/Properties.js
+++ portfolio-003-real-estate-/client/src/components/properties/Properties.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
-
+import { connect } from "react-redux";
+import MapPage from "./Map/MapPage";
 import styled from "styled-components";
+import PropertyContent from "./propertycontent/PropertyContent";
 const PropertiesStyle = styled.div`
   .property-container {
     border: 1px solid red;
+    display: flex;
+  }
+  .property-left {
+    border: 1px solid blue;
+    width: 60%;
+  }
+  .property-right {
+    border: 1px solid green;
+    width: 40vw;
+    height: 80vh;
   }
 `;
 
 class Properties extends Component {
   render() {
+    const filterData = this.props.data.filter((d, idx) => idx < 9);
+    console.log(filterData);
     return (
       <PropertiesStyle>
-        <Container className="property-container ">
-          <Row>
-            <Col xs="8" sm="4">
-              contents
-            </Col>
-            <Col xs="4" sm="4">
-              map
-            </Col>
-          </Row>
-        </Container>
+        <div className="property-container ">
+          <div className="property-left">
+            <PropertyContent />
+          </div>
+          <div className="property-right">
+            <MapPage />
+          </div>
+        </div>
       </PropertiesStyle>
     );
   }
 }
 
-export default Properties;
+const mapStateToProps = state => ({
+  data: state.propertyReducer.data
+});
+export default connect(mapStateToProps)(Properties);
---
+++ portfolio-003-real-estate-/client/src/components/properties/propertycontent/PropertyContent.js
@@ -0,0 +1,146 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Tooltip
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { getSingleListId } from "../../../store/actions/listingAction";
+import styled from "styled-components";
+const PropertyContentStyle = styled.div``;
+const images = [
+  {
+    src:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2016/03/city_9-1-525x328.jpg"
+  },
+  {
+    src:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2014/05/WPEstateImageAfter1013-525x328.jpg"
+  },
+  {
+    src:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2014/05/WPEstateImageAfter0913-525x328.jpg"
+  }
+];
+class PropertyContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    };
+  }
+  next = () => {
+    const nextIndex =
+      this.state.activeIndex === images.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+    this.setState((state, props) => {
+      return { activeIndex: (state.activeIndex = nextIndex) };
+    });
+  };
+  previous = () => {
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? images.length - 1
+        : this.state.activeIndex - 1;
+    this.setState((state, props) => {
+      return { activeIndex: (state.activeIndex = nextIndex) };
+    });
+  };
+  render() {
+    const { tooltipOpen, toggle, id } = this.props;
+    if (!images) {
+      return <span>listings currently not available</span>;
+    }
+    const slides = images.map((item, index) => {
+      let computedClass =
+        index === this.state.activeIndex ? "slide active" : "slide";
+      return (
+        <div key={index} className={computedClass}>
+          <div
+            className="carousel-parent"
+            style={{ backgroundImage: `url(${item.src})`, height: 320 }}
+            alt={item.altText}
+          />
+        </div>
+      );
+    });
+    return (
+      <PropertyContentStyle>
+        <Card>
+          <div className="carousel-info-parent">
+            <div className="carousel-info">
+              <p className="rentals ">Rentals</p>
+              <div>
+                <i className="far fa-heart" id="favorites-heart" />
+                <Tooltip
+                  placement="top"
+                  isOpen={tooltipOpen}
+                  target="favorites-heart"
+                  toggle={toggle}
+                >
+                  add to <br /> favorites
+                </Tooltip>
+              </div>
+              <div className="carousel-controls">
+                <i onClick={this.previous} className="fas fa-chevron-left" />
+                <i onClick={this.next} className="fas fa-chevron-right" />
+              </div>
+            </div>
+            {slides}
+          </div>
+          <Link
+            to={`/listing/${id}`}
+            onClick={() => this.props.getSingleListId(id)}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <CardBody>
+              <CardTitle>Luxury Villa In Rego Park</CardTitle>
+              <CardSubtitle> $ 2,100 / month</CardSubtitle>
+              <CardText>
+                Just steps away from QM2 express bus to Manhattan and local
+                buses; only minutes from the LIRR. Walki [more]
+              </CardText>
+
+              <div className="property_listing_details">
+                {" "}
+                <span className="infobath_unit_type2">
+                  <i className="fas fa-tint" />4
+                </span>{" "}
+                <span className="infogarage_unit_type2">
+                  <i className="fas fa-car" />
+                  10
+                </span>{" "}
+                <span className="infosize_unit_type2">
+                  <i className="far fa-map" />5 000,00 ft<sup>2</sup>
+                </span>
+              </div>
+
+              <div className="property-location">
+                <div className="property-name">
+                  <img
+                    src="https://losangeles.wpresidence.net/wp-content/uploads/2014/05/agent2-1-120x120.jpg"
+                    alt=""
+                  />
+                  <h5>Michael Suttherland</h5>
+                </div>
+                <span className="property-share">
+                  <i className="fas fa-share-alt" />
+                  <i className="fas fa-plus" />
+                </span>
+              </div>
+            </CardBody>
+          </Link>
+        </Card>
+      </PropertyContentStyle>
+    );
+  }
+}
+export default connect(
+  null,
+  { getSingleListId }
+)(PropertyContent);
